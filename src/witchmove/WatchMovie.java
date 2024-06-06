3618f6038317c1572e9e377748bd4e585550c0e2
Aluno <Aluno@10.85.198.36>
2024-06-06 16:51:39 -0300

    wit

---
+++ src/witchmove/WatchMovie.java
@@ -0,0 +1,14 @@
+public class WatchMovie {
+
+    public static void main(String args[]) {
+        int x = 12;
+        int y = 5;
+        if (x >= 12 && y == 5) {
+            System.out.println("Estou interesado em ver o filme");
+        }
+        else {
+            System.out.println("Nao estou interesado em ver o filme");
+        }
+
+    }
+}
